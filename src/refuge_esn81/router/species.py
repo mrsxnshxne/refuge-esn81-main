29a14af41659ead25644880ea4719be3d92696c1
RomanBonaparte <roman.cano04@gmail.com>
2025-11-13 14:49:20 +0100

    commentary

--- src/refuge_esn81/router/species.py
+++ src/refuge_esn81/router/species.py
@@ -8,11 +8,34 @@ from src.refuge_esn81.services.specieService import SpecieService
 speciesRouter = APIRouter(prefix="/api/species", tags=["species"])
 
 @speciesRouter.get("/", response_model=list[Species])
+
+
 async def get_species(db: Session = Depends(get_db)):
+    """
+    List all species
+
+    Args:
+        db: Session
+
+    Returns:
+        List[Species]
+
+    """
     service = SpecieService()
     return service.get_species(db)
 
 @speciesRouter.post("/", response_model=Species)
 def create_new_species(specie: SpeciesCreate, db: Session = Depends(get_db)):
+    """
+    Create a new specie
+
+    Args:
+        specie: Specie
+        db: Session
+
+    Returns:
+        Specie: new specie
+
+    """
     service = SpecieService()
     return service.create_specie(db, specie)
