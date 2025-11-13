0b9040fa053eb7b87e9a66aa1e74f24c3abcc57b
mrsxnshxne <jean.gabriel.trousseau12@gmail.com>
2025-11-13 14:02:06 +0100

    feat: update animal and species models with additional attributes and documentation

--- src/refuge_esn81/models/species.py
+++ src/refuge_esn81/models/species.py
@@ -1,11 +1,24 @@
+"""
+Species Model
+"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from refuge_esn81.database.database import Base
 
 class Species(Base):
+    """
+    Species Model representing an animal species in the refuge.
+
+    Attributes:
+        id (int): Primary key.
+        name (str): Name of the species.
+        animals (list): List of animals belonging to this species.
+    """
     __tablename__ = "species"
+
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(50), unique=True, index=True)
 
-    # Relation bidirectionnelle avec Animal
+    # relation with Animal table
     animals = relationship("Animal", back_populates="species")
